afcb6892991a68b015d429acc2343d3823b4dc16
Tejas <tejas.teju02@gmail.com>
2025-11-01 15:00:16 +0530

    path fix

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./app";
 import { ThemeProvider } from "./components/providers/theme-providers";
 import { Toaster } from "@smartgrow/ui/components/ui/sonner";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
